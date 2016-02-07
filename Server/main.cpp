7443a270ed4352236f7b77ed1191fae2b6e12e12
Gautam <gautam18abhi@gmail.com>
2016-02-07 18:05:33 -0500

    Initial Commit

---
+++ Server/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Server
+//
+//  Created by Gautam on 2/7/16.
+//  Copyright © 2016 Gautam. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
