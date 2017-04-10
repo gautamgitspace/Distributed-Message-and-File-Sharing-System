f78eb1cecaebba0eb2ee25ce6c94aa98ce302039
gautamgitspace <agautam2@buffalo.edu>
2017-04-10 10:01:17 -0700

    revert to indent

--- parser.py
+++ parser.py
@@ -3,27 +3,35 @@ import re
 def parseAUTHOR(output):
 	match = re.match('I, (.*?), have read and understood the course academic integrity policy.\\n', output)
 
-	if match: return "UBITNAME: "+match.group(1)
-	else: return "\033[91mFailed to parse: UBITNAME!\033[0m"
+	if match:
+		return "UBITNAME: "+match.group(1)
+	else:
+		return "\033[91mFailed to parse: UBITNAME!\033[0m"
 
 def parseIP(output):
 	match = re.match('IP:(.*?)\\n', output)
 
-	if match: return "IP address: "+match.group(1)
-	else: return "\033[91mFailed to parse: IP address!\033[0m"
+	if match:
+		return "IP address: "+match.group(1)
+	else:
+		return "\033[91mFailed to parse: IP address!\033[0m"
 
 def parsePORT(output):
 	match = re.match('PORT:(.*?)\\n', output)
 
-	if match: return "Listening port number: "+match.group(1)
-	else: return "\033[91mFailed to parse: Listening port number!\033[0m"
+	if match:
+		return "Listening port number: "+match.group(1)
+	else:
+		return "\033[91mFailed to parse: Listening port number!\033[0m"
 
 def printHosts(host_list):
 	display = ""
 	for host in host_list:
 		host_info = host.split()
-		if len(host_info) != 4: host_display = '\033[91mFailed to parse: Client information\033[0m'
-		else: host_display = 'ID:%s, Hostname:%s, IP address:%s, Listening Port number:%s' % (host_info[0], host_info[1], host_info[2], host_info[3])
+		if len(host_info) != 4:
+			host_display = '\033[91mFailed to parse: Client information\033[0m'
+		else:
+			host_display = 'ID:%s, Hostname:%s, IP address:%s, Listening Port number:%s' % (host_info[0], host_info[1], host_info[2], host_info[3])
 
 		display += host_display + '\n'
 
@@ -51,14 +59,18 @@ def parseLIST(output, type):
 def parseRELAYED(output):
 	match = re.match('msg from:(.*?), to:(.*?)\\n\[msg\]:(.*?)\\n', output)
 
-	if match: return 'From IP address:%s, To IP address:%s, Message:%s' % (match.group(1), match.group(2), match.group(3))
-	else: return "\033[91mFailed to parse: RELAYED message\033[0m"
+	if match:
+		return 'From IP address:%s, To IP address:%s, Message:%s' % (match.group(1), match.group(2), match.group(3))
+	else:
+		return "\033[91mFailed to parse: RELAYED message\033[0m"
 
 def parseRECEIVED(output):
 	match = re.match('msg from:(.*?)\\n\[msg\]:(.*?)\\n', output)
 
-	if match: return 'From IP address:%s, Message:%s' % (match.group(1), match.group(2))
-	else: return "\033[91mFailed to parse: RECEIVED message\033[0m"
+	if match:
+		return 'From IP address:%s, Message:%s' % (match.group(1), match.group(2))
+	else:
+		return "\033[91mFailed to parse: RECEIVED message\033[0m"
 
 def parseSTATISTICS(output):
 	hosts = output.rstrip('\n').split('\n')
@@ -68,9 +80,11 @@ def parseSTATISTICS(output):
 		display = ""
 		for host in hosts:
 			host_info = host.split()
-			if len(host_info) != 5: host_display = '\033[91mFailed to parse: Client information\033[0m'
-			else: host_display = 'ID:%s, Hostname:%s, #Messages-sent:%s, #Messages-received:%s, status:%s' % (host_info[0], host_info[1], host_info[2], host_info[3], host_info[4])
+			if len(host_info) != 5:
+				host_display = '\033[91mFailed to parse: Client information\033[0m'
+			else:
+				host_display = 'ID:%s, Hostname:%s, #Messages-sent:%s, #Messages-received:%s, status:%s' % (host_info[0], host_info[1], host_info[2], host_info[3], host_info[4])
 
 			display += host_display + '\n'
 
-		return display 
+		return display
